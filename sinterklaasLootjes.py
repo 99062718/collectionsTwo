58bb12963e91e8e05c7a01fe9c2df10a045250f1
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2021-10-13 10:21:56 +0200

    cleaned up sinterklaasLootjes.py
    got some critacism for using try-except and some unneeded lines. ill try not using those next time

--- sinterklaasLootjes.py
+++ sinterklaasLootjes.py
@@ -5,40 +5,24 @@ namen = []
 namen2 = []
 
 while choice != "nee":
-    try:
-        naam = input("Vul hier een unieke naam in:\n").lower()
-        isNaamInList = []
-        if len(namen) == 0:
-            namen.append(naam)
-            namen2.append(naam)    
-            triggerError() #this is not a real function. It exists purely to trigger an error
-        else:
-            for x in namen:
-                if naam != x:
-                    isNaamInList.append("nee")
-                else:
-                    isNaamInList.append("ja")
-        
-        for x in isNaamInList:
-            if x == "ja":
-                print("Deze naam zit al in de lijst!")
-                triggerError() #this is not a real function. It exists purely to trigger an error
-        
+    naam = input("Vul hier een unieke naam in:\n").lower()
+    if naam not in namen:
         namen.append(naam)
         namen2.append(naam)
+    else:
+        print("Deze naam heeft u al gebruikt!")
 
-        if len(namen) > 2:
-            choice = input("Wilt u nog een naam invullen (ja/nee)?\n").lower()
-    except:
-        pass
+    if len(namen) > 2:
+        choice = input("Wilt u nog een naam invullen (ja/nee)?\n").lower()
 
-while len(namen) > 0:
+while namen:
     naam1 = namen[random.randint(0, len(namen) - 1)]
     naam2 = namen2[random.randint(0, len(namen2) - 1)]
 
     if naam1 == naam2:
         if len(namen) == 1:
             print(naam1 + " is alleen :(")
+            exit()
     else:
         print(naam1 + " heeft " + naam2 + " als lootje!")
         namen.remove(naam1)
